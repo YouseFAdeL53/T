ad4362d1a1c1baf69c0de8b86829d01fbca2a8a4
YouseF AdeL <101376308+YouseFAdeL53@users.noreply.github.com>
2022-10-22 18:11:56 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// OnScrol Add Class to Navbar
+let myNavbar = document.querySelector(".navbar")
+
+window.addEventListener("scroll", () => {
+  myNavbar.classList.toggle("add-class", window.scrollY > 0);
+});
+
+
+window.onscroll = () => {
+  if (window.scrollY >= myNavbar.offsetTop) {
+    if (myNavbar.classList.contains("bg-white")) {
+      myNavbar.classList.remove("bg-white");
+    } else if (myNavbar.offsetTop === 0) {
+      myNavbar.classList.add("bg-white");
+    }
+  }
+  if (window.scrollY >= myStates.offsetTop) {
+    if (!started) {
+      mySpans.forEach((span) => myCounter(span));
+    }
+    started = true;
+  }
+}
+// Counter Nums
+let myStates = document.querySelector(".states");
+let mySpans = document.querySelectorAll(".states .box .edit");
+let started = false;
+
+function myCounter(el) {
+  let count = el.dataset.count;
+  let thisCounter = setInterval(() => {
+    el.textContent++
+    if (el.textContent === count) {
+      clearInterval(thisCounter)
+    }
+  }, 3000 / count)
+}
+
+
